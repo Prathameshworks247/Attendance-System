3fd70938008990fc08ad9770dd34c64e1e6332e1
Prathameshworks247 <prathameshworks247@gmail.com>
2026-02-06 23:52:53 +0530

    user model refines

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,6 +2,30 @@ import mongoose from "mongoose";
 
 const {Schema} = mongoose;
 
-const User = new Schema({
-    name : String 
+const UserSchema = new Schema({
+    
+    name : {
+        type: String,
+        required: true,
+        unique: [true, "username already in use"]
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: [true, "Email already in use"],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address']
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    role:{
+        type: String,
+        required: true
+    }
 })
+
+
+const UserModel = mongoose.model('UserModel', UserSchema)
+
+export default UserModel;
